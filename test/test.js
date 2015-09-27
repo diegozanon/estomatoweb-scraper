369ccf28eb94513c73a087555a8182e52bf0516d
zanon-io <zanon-io@users.noreply.github.com>
2015-09-27 11:39:36 -0300

    Calculating pacient age; Adding automated tests to check dates.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('chai-datetime'));
 var sinon = require('sinon');
 var scrap = require('../lib/scrap');
 var constants = require('../lib/constants');
@@ -79,13 +81,17 @@ describe('utils', function() {
             var testsTrue = [1, 15, '32'];
 
             testsTrue.forEach(function(test){
-                expect(utils.isPositiveInt(test)).equal(true);
+                var isPositive = utils.isPositiveInt(test);
+                var expected = true;
+                expect(isPositive).equal(expected);
             });
 
             var testsFalse = [0, -1, '0', '-7', 3.14, undefined, '', 'test1'];
 
             testsFalse.forEach(function(test){
-                expect(utils.isPositiveInt(test)).equal(false);
+                var isPositive = utils.isPositiveInt(test);
+                var expected = false;
+                expect(isPositive).equal(expected);
             });
         });
     });
@@ -96,16 +102,55 @@ describe('utils', function() {
 
             // Currently, the website uses SITE_PATIENTS_PERPAGE = 15
             var tests = [
-                { value:  0, pages: 0},
-                { value:  1, pages: 1},
-                { value: 14, pages: 1},
-                { value: 15, pages: 1},
-                { value: 16, pages: 2},
-                { value: 47, pages: 4}
+                { value:  0, expected: 0},
+                { value:  1, expected: 1},
+                { value: 14, expected: 1},
+                { value: 15, expected: 1},
+                { value: 16, expected: 2},
+                { value: 47, expected: 4}
             ];
 
             tests.forEach(function(test){
-                expect(utils.getNbOfPages(test.value)).equal(test.pages);
+                var nbOfPages = utils.getNbOfPages(test.value);
+                expect(nbOfPages).equal(test.expected);
+            });
+        });
+    });
+
+    describe('#convertEstomatoDateToJSDate()', function() {
+
+        it('should convert EstomatoWeb date to JavaScript date', function() {
+
+            var tests = [
+                { value: '31/ 5 /1985', expected: new Date('1985-5-31') },
+                { value: '31/ 12 /1985', expected: new Date('1985-12-31') },
+                { value: '5/ 5 /1985', expected: new Date('1985-5-5') },
+                { value: '5/ 12 /1985', expected: new Date('1985-12-5') },
+                { value: '31/ 5 /1900', expected: new Date('1900-5-31') }
+            ];
+
+            tests.forEach(function(test){
+                var date = utils.convertEstomatoDateToJSDate(test.value);
+                expect(date).to.equalDate(test.expected);
+            });
+        });
+    });
+
+    describe('#getAge()', function() {
+
+        it('should return the difference in years', function() {
+
+            var tests = [
+                { date1: new Date('1954-12-1'), date2: new Date('1982-3-13'), expected: 27},
+                { date1: new Date('1954-3-1'), date2: new Date('1982-3-13'), expected: 28},
+                { date1: new Date('1954-3-12'), date2: new Date('1982-3-13'), expected: 28},
+                { date1: new Date('1954-3-13'), date2: new Date('1982-3-13'), expected: 28},
+                { date1: new Date('1954-3-14'), date2: new Date('1982-3-13'), expected: 27}
+            ];
+
+            tests.forEach(function(test){
+                var diff = utils.getAge(test.date1, test.date2);
+                expect(diff).equal(test.expected);
             });
         });
     });
